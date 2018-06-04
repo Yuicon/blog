b79ef32a14851d9caf1b9400a7e03c85195d975f
Yuicon <910722178@qq.com>
2018-06-04 16:40:20 +0800

    perf: 添加注释

--- algorithm/src/SelectionSort.java
+++ algorithm/src/SelectionSort.java
@@ -7,6 +7,9 @@ import java.util.List;
  */
 public class SelectionSort {
 
+    /**
+     * 待排序的列表
+     */
     private static List<Integer> arr = Arrays.asList(9, 1, 4, 5, 7, 2, 8, 6, 3, 9, 2);
 
     public static void main(String[] args) {
@@ -14,17 +17,19 @@ public class SelectionSort {
         int tmp;
         for (int i = 0; i < arr.size() - 1; i++) {
             minIndex = i;
+            // 找出未排序的数据中最小的数据
             for (int j = i + 1; j < arr.size(); j++) {
                 if (arr.get(j) < arr.get(minIndex)) {
+                    // 将 minIndex 指向最小数据的索引
                     minIndex = j;
                 }
             }
+            // 交换数据
             tmp = arr.get(i);
             arr.set(i, arr.get(minIndex));
             arr.set(minIndex, tmp);
         }
         System.out.println(arr);
     }
-
 
 }
