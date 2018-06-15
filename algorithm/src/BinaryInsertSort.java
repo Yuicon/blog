68a3b7bc220d3f56dc662a75595cb5f03e5923d4
Yuicon <910722178@qq.com>
2018-06-15 15:55:12 +0800

    BinaryInsertSort

--- algorithm/src/BinaryInsertSort.java
+++ algorithm/src/BinaryInsertSort.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 /**
  * @author Yuicon
- * @Date 2018/6/5
+ * @Date 2018/6/15
  */
-public class InsertSort {
+public class BinaryInsertSort {
 
     /**
      * 待排序的列表
@@ -17,15 +17,22 @@ public class InsertSort {
         for (int i = 1; i < list.size(); i++) {
             //保存当前位置 i 的元素，其中[0, i - 1]已经有序
             tmp = list.get(i);
-            int j;
+            int mid, left = 0, right = i - 1;
+            //用二分法找合适的位置
+            while (left <= right) {
+                mid = (left + right) / 2;
+                if (tmp < list.get(mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
             //在已排序的元素中将大于 i 的元素都后移一位
-            for (j = i; j > 0 && tmp < list.get(j - 1); j--) {
-                int t = list.get(j);
-                list.set(j, list.get(j - 1));
-                list.set(j - 1, t);
+            for (int j = i - 1; j >= left; j--) {
+                list.set(j + 1, list.get(j));
             }
             //插入到合适的位置
-            list.set(j, tmp);
+            list.set(left, tmp);
         }
         System.out.println(list);
     }
