d42f950ba310b26ff6bbbe9934d72b730ce7d5ac
Yuicon <910722178@qq.com>
2018-09-04 16:01:47 +0800

    add entry details

--- client/src/Entry.js
+++ client/src/Entry.js
@@ -14,7 +14,18 @@ class Entry extends Component {
 
         return (
             <div className="entry">
-                <p onClick={this.handleOnClick}>{this.props.article.title}</p>
+                <div>
+                    <p className="article-title" onClick={this.handleOnClick}>{this.props.article.title}</p>
+                </div>
+                <div className="details">
+                    <span>创建于：{this.props.article.createdAt.replace(/Z/g,' ').replace(/T/g, ' ')} · </span>
+                    <span>最后更新于：{this.props.article.updatedAt.replace(/Z/g,' ').replace(/T/g, ' ')} · </span>
+                    <a href={`https://github.com/${this.props.article.gitUserName}/${this.props.article.repositoryName}`}
+                       target="_blank"
+                    >
+                        仓库地址
+                    </a>
+                </div>
             </div>
         );
     }
