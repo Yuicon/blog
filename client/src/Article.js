da2c0ab17338326b1c278752f1a2faffb2d0ad20
Yuicon <910722178@qq.com>
2018-09-11 17:21:08 +0800

    gitment init

--- client/src/Article.js
+++ client/src/Article.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import {api} from "./api";
+import 'gitment/style/default.css'
+import Gitment from 'gitment'
 
 /**
  * @author Yuicon
@@ -17,7 +19,18 @@ class Article extends Component {
 
     async componentDidMount() {
         const article = await api.getArticleById(this.props.match.params.id);
-        this.setState({article, error: "<h1>没有文章哦！</h1>"});
+        this.setState({article, error: "<h1>没有文章哦！</h1>"}, () => {
+            const gitment = new Gitment({
+                id: article.id,
+                owner: article.gitUserName,
+                repo: article.repositoryName,
+                oauth: {
+                    client_id: '1f4bdb550130e267946f',
+                    client_secret: '1b48fcde26285d26e4b71193bff6f1d809031a03',
+                },
+            });
+            gitment.render('container');
+        });
         console.log(article);
     }
 
@@ -28,6 +41,9 @@ class Article extends Component {
                 <h1>{this.state.article.title}</h1>
                 <article className="markdown-body" dangerouslySetInnerHTML={{__html: this.state.article.body || this.state.error}}>
                 </article>
+                <div className="comment-full">
+                    <div id="container" className="comment-content"/>
+                </div>
             </div>
         );
     }
