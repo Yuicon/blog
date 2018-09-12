060aa4a51b368edd4ab950e6d73e088106585aa2
Yuicon <910722178@qq.com>
2018-09-12 11:22:05 +0800

    文章列表优化

--- client/src/Entry.js
+++ client/src/Entry.js
@@ -15,11 +15,15 @@ class Entry extends Component {
         return (
             <div className="entry">
                 <div>
-                    <p className="article-title" onClick={this.handleOnClick}>{this.props.article.title}</p>
+                    <a className="article-title"
+                       target="_blank"
+                       href={`/articles/${this.props.article.id}`}>
+                        <p>{this.props.article.title}</p>
+                    </a>
                 </div>
                 <div className="details">
-                    <span>创建于：{this.props.article.createdAt.replace(/Z/g,' ').replace(/T/g, ' ')} · </span>
-                    <span>最后更新于：{this.props.article.updatedAt.replace(/Z/g,' ').replace(/T/g, ' ')} · </span>
+                    <span>创建于：{this.props.article.createdAt.replace(/Z/g, ' ').replace(/T/g, ' ')} · </span>
+                    <span>最后更新于：{this.props.article.updatedAt.replace(/Z/g, ' ').replace(/T/g, ' ')} · </span>
                     <a href={`https://github.com/${this.props.article.gitUserName}/${this.props.article.repositoryName}`}
                        target="_blank"
                     >
