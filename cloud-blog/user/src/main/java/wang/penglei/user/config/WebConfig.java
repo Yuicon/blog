aa6c355693319964afadeaacbcc4cb9370b17479
Yuicon <910722178@qq.com>
2019-01-13 18:07:26 +0800

    fix: 配置

---
+++ cloud-blog/user/src/main/java/wang/penglei/user/config/WebConfig.java
@@ -0,0 +1,20 @@
+package wang.penglei.user.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * @author Yuicon
+ */
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+
+}
