48e3f7530f16202c4712cc8ec9ed28dee201250d
Yuicon <910722178@qq.com>
2019-10-11 13:34:23 +0800

    feat: 评论的展示

--- client/src/component/article/CommentBase.js
+++ client/src/component/article/CommentBase.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Editor from "./Editor";
 import {Button, Collapse, Comment, message, Modal, Tooltip} from "antd";
 import {commentApi} from "../../api/articleApi";
-import BraftEditor from 'braft-editor'
+import BraftEditor from "braft-editor";
 import moment from "moment";
 
 const {Panel} = Collapse;
@@ -30,7 +30,20 @@ export default class CommentBase extends React.Component {
             message.error(body.message);
             return;
         }
-        this.setState({comments: body.data.records});
+        const comments = body.data.records;
+        const floors = {};
+        comments.filter(comment => comment.quoteId === 0).forEach(comment => {
+            floors[comment.id] = [comment];
+        });
+        comments.filter(comment => comment.quoteId !== 0).reverse().forEach(comment => {
+            floors[comment.id] = floors[comment.quoteId].concat();
+            floors[comment.id].push(comment);
+        });
+        const commentList = [];
+        comments.forEach(comment => {
+            commentList.push(floors[comment.id]);
+        });
+        this.setState({comments: commentList});
     };
 
     sentComment = async () => {
@@ -39,7 +52,7 @@ export default class CommentBase extends React.Component {
             return;
         }
 
-        if (this.state.content.length > 1400) {
+        if (this.state.content.length > 5400) {
             message.warn("字数过多!");
             return;
         }
@@ -73,23 +86,33 @@ export default class CommentBase extends React.Component {
                         {
                             this.state.comments.length > 0 && <div style={{border: '1px solid gainsboro'}}>
                                 {
-                                    this.state.comments.map(comment => {
-                                        const editorState = BraftEditor.createEditorState(comment.content);
-                                        const content = editorState.toHTML();
-                                        return <Comment
-                                            style={{border: '1px solid #fff4f4b5'}}
-                                            actions={[<span key="comment-nested-reply-to"
-                                                            onClick={this.replyHandle.bind(this, comment)}>回复</span>]}
-                                            key={comment.id}
-                                            author={<a>{comment.accountName}</a>}
-                                            content={<article dangerouslySetInnerHTML={{__html: content}}
-                                                              style={{textAlign: 'left'}}/>}
-                                            datetime={
-                                                <Tooltip title={moment().format(comment.createTime)}>
-                                                    <span>{moment(comment.createTime).fromNow()}</span>
-                                                </Tooltip>
+                                    this.state.comments.map((commentList, i) => {
+                                        return <div key={i} style={{padding: "30px"}}>
+                                            {
+                                                commentList.map((comment, index) => {
+                                                    const editorState = BraftEditor.createEditorState(comment.content);
+                                                    const content = editorState.toHTML();
+                                                    const style = {border: '1px solid #fff4f4b5', textAlign: "left"};
+                                                    if (index !== (commentList.length - 1)) {
+                                                        style.backgroundColor = "#fff8dc82";
+                                                    }
+                                                    return <Comment
+                                                        style={style}
+                                                        actions={[<span key="comment-nested-reply-to"
+                                                                        onClick={this.replyHandle.bind(this, comment)}>回复</span>]}
+                                                        key={comment.id}
+                                                        author={<a>{`${comment.floor}L - ${comment.accountName}`}</a>}
+                                                        content={<article dangerouslySetInnerHTML={{__html: content}}
+                                                                          style={{textAlign: 'left'}}/>}
+                                                        datetime={
+                                                            <Tooltip title={moment().format(comment.createTime)}>
+                                                                <span>{moment(comment.createTime).fromNow()}</span>
+                                                            </Tooltip>
+                                                        }
+                                                    />;
+                                                })
                                             }
-                                        />;
+                                        </div>;
                                     })
                                 }
                             </div>
@@ -109,6 +132,7 @@ export default class CommentBase extends React.Component {
                     visible={this.state.visible}
                     onOk={this.sentComment}
                     onCancel={() => this.setState({visible: false, quoteId: 0})}
+                    afterClose={() => this.setState({visible: false, quoteId: 0})}
                 >
                     <Editor onChange={html => this.setState({content: html})}/>
                 </Modal>
